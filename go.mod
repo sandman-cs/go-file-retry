1e375032620b1262ea47ae9ad39364d9f0abe165
Chris <34791945+sandman-cs@users.noreply.github.com>
2020-03-04 15:51:06 -0600

    1.0 - Not working yet

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/sandman-cs/go-file-retry
+
+go 1.14
+
+require (
+	github.com/patrickmn/go-cache v2.1.0+incompatible
+	github.com/sirupsen/logrus v1.4.2
+)
--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net"
-	"net/mail"
 	"os"
 	"os/signal"
 	"strconv"
@@ -14,8 +13,6 @@ import (
 	"time"
 
 	log "github.com/sirupsen/logrus"
-
-	"github.com/scorredoira/email"
 )
 
 type emailPayload struct {
@@ -23,20 +20,12 @@ type emailPayload struct {
 	attach  bool
 }
 
-var (
-	conf                                             configuration
-	alive, gemail, bemail, dmemail, qemail, dupEmail int
-	emailPub                                         []chan emailPayload
-	closeReceived                                    bool
-)
-
 //checkError function
 func checkError(err error) {
 	if err != nil {
 		log.WithFields(log.Fields{
 			"app":    conf.AppName,
 			"ver":    conf.AppVer,
-			"Inst":   conf.InstanceID,
 			"server": conf.ServerName,
 		}).Error(fmt.Sprint(err))
 	}
@@ -74,7 +63,6 @@ func sendMessage(msg string) {
 	log.WithFields(log.Fields{
 		"app":    conf.AppName,
 		"ver":    conf.AppVer,
-		"Inst":   conf.InstanceID,
 		"server": conf.ServerName,
 	}).Info(msg)
 }
@@ -85,7 +73,6 @@ func sendDebugMessage(msg string) {
 	log.WithFields(log.Fields{
 		"app":    conf.AppName,
 		"ver":    conf.AppVer,
-		"Inst":   conf.InstanceID,
 		"server": conf.ServerName,
 	}).Debug(msg)
 }
@@ -96,7 +83,6 @@ func sendWarnMessage(msg string) {
 	log.WithFields(log.Fields{
 		"app":    conf.AppName,
 		"ver":    conf.AppVer,
-		"Inst":   conf.InstanceID,
 		"server": conf.ServerName,
 	}).Warn(msg)
 }
@@ -107,7 +93,6 @@ func sendMetrics(good int, bad int, del int) {
 	log.WithFields(log.Fields{
 		"app":    conf.AppName,
 		"ver":    conf.AppVer,
-		"Inst":   conf.InstanceID,
 		"server": conf.ServerName,
 		"good":   good,
 		"bad":    bad,
@@ -125,7 +110,6 @@ func main() {
 		sig := <-sigs
 		sendMessage("Exiting Program, recieved: ")
 		sendMessage(fmt.Sprintln(sig))
-		UnRegister(conf.DbServer, conf.DbUser, conf.DbPwd, conf.HealthID, conf.InstanceID)
 
 		//Add Code here to send message to kill all input threads.
 		closeReceived = true
@@ -137,12 +121,11 @@ func main() {
 				if gemail == 0 {
 					time.Sleep(time.Second * 5)
 					sendMessage("Decrementing Send Thread Count.")
-					wg.Done()
+
 				}
 			}
 		}()
 		sendMessage("Waiting for send treads to finish...")
-		wg.Wait()
 		os.Exit(0)
 	}()
 
@@ -185,11 +168,6 @@ func workLoop(workDir string, chanID int) {
 							//err := os.Remove(sFileName)
 							//checkError(err)
 							bemail++
-						} else if stringContains(sLine[0], conf.DelMatch) {
-							sendMessage(fmt.Sprintf("Match found in \"%s\", deleting %s", sLine[0], sFileName))
-							//err := os.Remove(sFileName)
-							//checkError(err)
-							dmemail++
 						} else if emailHashChecker(string(dat)) {
 							dupEmail++
 							sendMessage(fmt.Sprintf("Duplicate found in \"%s\", deleting %s", sLine[0], sFileName))
@@ -203,7 +181,7 @@ func workLoop(workDir string, chanID int) {
 							if strings.Contains(sFileName, ".dtt") {
 								tEmail.attach = true
 							}
-							emailPub[chanID] <- tEmail
+
 							//err := os.Remove(sFileName)
 							//checkError(err)
 							qemail++
@@ -222,137 +200,10 @@ func workLoop(workDir string, chanID int) {
 	}
 }
 
-func sendEmailChannel(input chan emailPayload, srv emailSrv) {
-	defer func() {
-		if r := recover(); r != nil {
-			sendMessage(fmt.Sprintf("Recovered from error: %s ", r))
-		}
-	}()
-
-	var szTemp emailPayload
-
-	sendMessage(fmt.Sprint("Connecting to Server: ", srv))
-	wg.Add(1)
-	for {
-		szTemp = <-input
-		if szTemp.attach {
-			sendEmailWithAttach(szTemp.payload, srv)
-		} else {
-			sendEmailNoAttach(szTemp.payload, srv)
-		}
-		gemail++
-	}
-}
-
-func sendEmailWithAttach(payload []string, srv emailSrv) {
-	defer func() {
-		if r := recover(); r != nil {
-			sendMessage(fmt.Sprintf("Recovered from error: %s ", r))
-		}
-	}()
-
-	// compose the message
-	body := ""
-	for i := 4; i < len(payload); i++ {
-		body += payload[i] + "\n"
-	}
-	m := email.NewMessage(payload[2], body)
-	m.From = mail.Address{Name: payload[1], Address: payload[1]}
-	m.To = strings.Split(payload[0], ";")
-
-	// add attachments
-	if strings.Contains(payload[3], "<attachlist>") {
-		attchList := getXMLAttach(payload[3])
-		for _, attach := range attchList {
-			fi, e := os.Stat(attach)
-			checkError(e)
-			if e == nil && fi.Size() <= conf.AttachSize {
-				if err := m.Attach(attach); err != nil {
-					checkError(err)
-				}
-			}
-
-			if fi.Size() > conf.AttachSize {
-				err := os.Remove(attach)
-				checkError(err)
-				sendMessage(fmt.Sprint("Error: attachment too large: ", fi.Name()))
-			}
-		}
-	} else {
-		fi, e := os.Stat(payload[3])
-
-		if e == nil && fi.Size() <= conf.AttachSize {
-			if err := m.Attach(payload[3]); err != nil {
-				log.Println(err)
-			}
-		}
-
-		if fi.Size() > conf.AttachSize {
-			err := os.Remove(payload[3])
-			checkError(err)
-			sendMessage(fmt.Sprint("Error: attachment too large: ", fi.Name()))
-
-		}
-
-	}
-
-	if err := email.Send(srv.EmailSrv+":"+srv.EmailPrt, nil, m); err != nil {
-		log.Println(err)
-	}
-}
-
-func sendEmailNoAttach(payload []string, srv emailSrv) {
-	// compose the message
-	body := ""
-	for i := 3; i < len(payload); i++ {
-		body += payload[i] + "\n"
-	}
-	m := email.NewMessage(payload[2], body)
-	m.From = mail.Address{Name: payload[1], Address: payload[1]}
-	m.To = strings.Split(payload[0], ";")
-
-	if err := email.Send(srv.EmailSrv+":"+srv.EmailPrt, nil, m); err != nil {
-		checkError(err)
-	}
-}
-
-func stringToLines(s string) []string {
-	var lines []string
-
-	scanner := bufio.NewScanner(strings.NewReader(s))
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
-		sendMessage(fmt.Sprintln("Error reading standard input:", err))
-	}
-	return lines
-}
-
-// stringContains checkes the srcString for any matches in the
-// list, which is an array of strings.
-func stringContains(a string, list []string) bool {
-	for _, b := range list {
-		if strings.Contains(a, b) {
-			return true
-		}
-	}
-	return false
-}
-
 func healthCheck() {
 	sendMessage("Starting health check thread...")
-	register := true
+
 	for {
-		if register {
-			HealthRegister(conf.DbServer, conf.DbUser, conf.DbPwd, conf.HealthID, conf.InstanceID, conf.ServerName, conf.AppName+" "+conf.AppVer)
-			register = false
-		} else if alive > 0 {
-			HealthCheck(conf.DbServer, conf.DbUser, conf.DbPwd, conf.HealthID, conf.InstanceID)
-		} else {
-			sendMessage("Failed Health Check...")
-		}
 		payload := "{\"app\": \"emailer\",\"good_email\":" + strconv.Itoa(gemail) + ",\"bad_email\":" + strconv.Itoa(bemail) + ",\"del_email\":" + strconv.Itoa(dmemail) + ",\"dup_email\":" + strconv.Itoa(dupEmail) + "}"
 		sendUDPMessage(payload)
 
